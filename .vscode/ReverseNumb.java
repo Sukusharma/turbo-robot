3803b865b94560d37a512b05eb9ac659317f333d
Sukusharma <139940860+Sukusharma@users.noreply.github.com>
2024-10-23 19:03:33 +0530

    Created to Reverse any number

---
+++ .vscode/ReverseNumb.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class ReverseNumb{
+public static void revNum(int number){
+  if ( number<10){
+    System.out.print(number);
+    return;
+  }
+  else{
+    System.out.print(number%10);
+    revNum(number/10);
+  }
+
+}
+
+  public static void  main(String[] args){
+    System.out.print("Enter the number that you want to reverse: ");  
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
+    System.out.print("The reverse of the given number is: ");  
+    //method calling  
+    revNum(num);
+  }
+}
+
